dd1653eb12906badad4b2768d6fb158e7176d718
Andrea Scuto <scutoandrea6@gmail.com>
2025-08-30 19:43:29 +0200

    Data structured for ChatService, including DTOs for OpenRouter

---
+++ ChatService.Domain/Models/Chat.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Domain.Models;
+
+public class Chat
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+
+    public required List<Message?> Messages { get; set; }
+}
---
+++ ChatService.Domain/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Enums;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Domain.Models;
+
+public class Message
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+
+    public required Role Role { get; set; }
+    public required string Content { get; set; }
+}
