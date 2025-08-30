461f5ad146a27ba4ae7c9ca1f5b63763eac5e8c3
Andrea Scuto <scutoandrea6@gmail.com>
2025-08-30 13:01:52 +0200

    Added initial configuration (structure & models)

---
+++ AiModel.Domain/Models/AiModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AiModel.Domain.Models;
+
+public class AiModel
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public long ReleaseDate { get; set; }
+    public string? Description { get; set; }
+    public long ContextLength { get; set; }
+    public required Pricing Pricing { get; set; }
+}
---
+++ AiModel.Domain/Models/Pricing.cs
@@ -0,0 +1,7 @@
+﻿namespace AiModel.Domain.Models;
+
+public class Pricing
+{
+    public double PricePrompt { get; set; }
+    public double PriceCompletion { get; set; }
+}
